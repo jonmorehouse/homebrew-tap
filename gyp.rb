d722991356754f4a822642d31ead1b3faa37d0fd
Jon Morehouse <morehousej09@gmail.com>
2013-12-11 15:28:09 -0800

    Update gyp help

--- gyp.rb
+++ gyp.rb
@@ -3,13 +3,15 @@ require 'formula'
 class Gyp < Formula
 
 	# homebrew should automatically know what to do svn repos
-	url ""
+	url "svn checkout http://gyp.googlecode.com/svn/trunk/"
 
 	# declare dependencies
 	depends_on "make" => :build
 	
 	# initialize the installation of this
 	def install
+
+		puts "HELLO WORLD"
 		
 		# build gyp -- we will copy over the correct files later on
 		system "make dependencies"
