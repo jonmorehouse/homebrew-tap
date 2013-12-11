9201f8aaf2b9b5fbe44727fb1e8f353651f1a53b
Jon Morehouse <morehousej09@gmail.com>
2013-12-11 15:32:54 -0800

    Fix gyp svn version

--- gyp.rb
+++ gyp.rb
@@ -3,20 +3,20 @@ require 'formula'
 class Gyp < Formula
 
 	# homebrew should automatically know what to do svn repos
-	url "svn checkout http://gyp.googlecode.com/svn/trunk/"
+	url "http://gyp.googlecode.com/svn/trunk/"
 
 	# declare dependencies
-	depends_on "make" => :build
+	#depends_on "make" => :build
 	
 	# initialize the installation of this
-	def install
+	#def install
 
-		puts "HELLO WORLD"
+		#puts "HELLO WORLD"
 		
-		# build gyp -- we will copy over the correct files later on
-		system "make dependencies"
+		## build gyp -- we will copy over the correct files later on
+		#system "make dependencies"
 		
-		# now copy the gyp files to the correct location on your machine
+		## now copy the gyp files to the correct location on your machine
 
-	end
+	#end
 end
