921cab4018609ea047a77194e1748a978ea310e5
Jon Morehouse <morehousej09@gmail.com>
2013-12-24 21:23:57 -0800

    Update local development settings etc

--- ios-sim.rb
+++ ios-sim.rb
@@ -2,22 +2,19 @@ require 'formula'
 
 #In the future, this module should custom build ios-sim using ruby / rake
 #for now, simply use the latest npm and use nsm
-class IosSim < Formula
+#class IosSim < Formula
 	
-	url "https://github.com/phonegap/ios-sim", :using => :git	
-	version "1.0"
+	#url "https://github.com/phonegap/ios-sim", :using => :git	
+	#version "1.0"
 
-	depends_on "nave"
+	##depends_on "ruby"
 
-	def install
+	#def install
 
-		# make sure we have the latest npm
-		latest = system "nave latest"
+		## now check the version
+		##system "npm install -g ios-sim"
 
-		puts latest
-		# run npm install -g with the latest npm
-
-	end
+	#end
 
 
-end
+#end
